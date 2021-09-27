d78e86d5c844a8adccdb6d4154cf8971e8ce9eb1
Lee Vang <lvang_dm@yahoo.com>
2021-09-27 17:34:50 -0500

    Initial Upload

---
+++ APIRequest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace APIRequest
+{
+    class Program
+    {
+        readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args) 
+        {
+            Program program = new Program();
+            await program.GetUserInfo();
+        }
+        private async Task GetUserInfo() 
+        {
+            string response = await client.GetStringAsync("https://alexdmeyer.com/codetest/users.json");
+
+            List<Objects> UserList = JsonConvert.DeserializeObject<List<Objects>>(response);
+
+            //May need to create Temp folder if not already there
+            using (StreamWriter file = File.CreateText(@"C:\Temp\results.csv"))
+            {
+                foreach (var item in UserList) 
+            {
+                    if (item.AccountEnabled == "true" && !item.DisplayName.Contains("S"))
+                        {
+                        
+                        Console.WriteLine($"{item.UserPrincipalName} {item.DisplayName} {item.AccountEnabled}");
+
+                        JsonSerializer serializer = new JsonSerializer();
+                        
+                        serializer.Serialize(file, $"Principal Name: {item.UserPrincipalName} Display Name: {item.DisplayName} Account Enabled: {item.AccountEnabled}");
+                        }
+                    }
+            }
+        }
+        class Objects 
+        {
+            public string UserPrincipalName { get; set; }
+            public string DisplayName { get; set; }
+            public string AccountEnabled { get; set; }
+        }
+    }
+}

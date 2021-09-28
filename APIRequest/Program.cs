9463668a07f150abcbed41393bf2fb26bb52ff3b
Alex Meyer <ameyer505@gmail.com>
2021-09-28 10:58:31 -0500

    Changed check for DisplayName to perform a ToUpper() as Contains method is case sensitive
    For CSV file output, added header row since we know the columns being returned and made it so that the entries weren't written all on one line
    
    Changed type of AccountEnabled from string to bool

--- APIRequest/Program.cs
+++ APIRequest/Program.cs
@@ -11,12 +11,12 @@ namespace APIRequest
     {
         readonly HttpClient client = new HttpClient();
 
-        static async Task Main(string[] args) 
+        static async Task Main(string[] args)
         {
             Program program = new Program();
             await program.GetUserInfo();
         }
-        private async Task GetUserInfo() 
+        private async Task GetUserInfo()
         {
             string response = await client.GetStringAsync("https://alexdmeyer.com/codetest/users.json");
 
@@ -25,25 +25,24 @@ namespace APIRequest
             //May need to create Temp folder if not already there
             using (StreamWriter file = File.CreateText(@"C:\Temp\results.csv"))
             {
-                foreach (var item in UserList) 
-            {
-                    if (item.AccountEnabled == "true" && !item.DisplayName.Contains("S"))
-                        {
-                        
-                        Console.WriteLine($"{item.UserPrincipalName} {item.DisplayName} {item.AccountEnabled}");
+                file.WriteLine("Principal Name, Display Name, Account Enabled");
 
-                        JsonSerializer serializer = new JsonSerializer();
-                        
-                        serializer.Serialize(file, $"Principal Name: {item.UserPrincipalName} Display Name: {item.DisplayName} Account Enabled: {item.AccountEnabled}");
-                        }
+                foreach (var item in UserList)
+                {
+                    if (item.AccountEnabled == true && !item.DisplayName.ToUpper().Contains("S"))
+                    {
+                        Console.WriteLine($"{item.UserPrincipalName}, {item.DisplayName}, {item.AccountEnabled}");
+                        file.WriteLine($"{item.UserPrincipalName}, {item.DisplayName}, {item.AccountEnabled}");
+
                     }
+                }
             }
         }
-        class Objects 
+        class Objects
         {
             public string UserPrincipalName { get; set; }
             public string DisplayName { get; set; }
-            public string AccountEnabled { get; set; }
+            public bool AccountEnabled { get; set; }
         }
     }
 }
